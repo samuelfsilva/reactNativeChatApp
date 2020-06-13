c81ffc66063f1b3bd8456346e5b6a9b371fa50bf
samuelfsilva <samukfer@gmail.com>
2020-06-13 11:25:37 -0300

    Correções no gradlew do projeto.

--- android/app/src/main/java/com/reactnativechatapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativechatapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.exemplo;
+package com.reactnativechatapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "exemplo";
+    return "reactNativeChatApp";
   }
 }
